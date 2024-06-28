e6e30ef6d601404be01563ad1c876c0dad409aee
Purim Chaiyawong <rim-mc@Purims-MacBook-Pro.local>
2024-06-29 02:08:03 +0700

    add

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/users')
+      .then(response => response.json())
+      .then(data => setUsers(data));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Users</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.name} ({user.age} years old)</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
